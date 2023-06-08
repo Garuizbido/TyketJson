04c55fb72a597c6d9500c8161a4130f30852e985
Garuizbido <garuizbido@gmail.com>
2023-06-08 17:47:24 -0400

    first commit

---
+++ TyketJson/Models/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace TyketJson.Models
+{
+	
+		
+        public class Address
+        {
+            public string formatted_address { get; set; }
+            public string street_address { get; set; }
+            public string city_name { get; set; }
+            public string state_name { get; set; }
+            public string country { get; set; }
+            public string country_name { get; set; }
+            public string zipcode { get; set; }
+        }
+
+        public class Person
+        {
+            public string id { get; set; }
+            public string first_name { get; set; }
+            public string last_name { get; set; }
+            public string full_name { get; set; }
+            public string username { get; set; }
+            public string email { get; set; }
+            public string phone_number { get; set; }
+            public int age { get; set; }
+            public string date_of_birth { get; set; }
+            public double yearly_income { get; set; }
+            public int followers { get; set; }
+            public int following { get; set; }
+            public string biography { get; set; }
+            public string website { get; set; }
+            public Address address { get; set; }
+        }
+}
+
---
+++ TyketJson/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.IO.Compression;
+using TyketJson.Models;
+using Newtonsoft.Json;
+
+public class Program
+{
+    private static async Task download_and_decompress(string url, string path)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            using (HttpResponseMessage response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead))
+            {
+                response.EnsureSuccessStatusCode();
+
+                using (Stream gzipStream = await response.Content.ReadAsStreamAsync())
+                using (Stream decompressedStream = new GZipStream(gzipStream, CompressionMode.Decompress))
+                using (FileStream fileStream = new FileStream(path, FileMode.Create, FileAccess.Write))
+                {
+                    await decompressedStream.CopyToAsync(fileStream);
+                }
+            }
+        }
+    }
+
+    public static async Task Main()
+    {
+        string url = "https://staging.media.tyket.app/dotnet-test-data.json.gz";
+        string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "dotnet-test-data.json");
+
+        await download_and_decompress(url, path);
+
+        string text = File.ReadAllText(path);
+
+        var data = JsonConvert.DeserializeObject<Person[]>(text);
+
+        Console.WriteLine("Total items: " + data.Count());
+
+        Console.WriteLine();
+
+        Console.WriteLine("Total items in Florida: " + data.Count(item => item.address.state_name == "Florida"));
+
+        Console.WriteLine();
+
+        var highestIncomePerson = data?
+            .Where(item => item.address.state_name == "Florida")
+            .OrderByDescending(item => item.yearly_income)
+            .FirstOrDefault();
+
+        Console.WriteLine("Person with highest income in Florida:");
+        Console.WriteLine("ID: " + highestIncomePerson.id);
+        Console.WriteLine("Full Name: " + highestIncomePerson.full_name);
+        Console.WriteLine("Income: " + highestIncomePerson.yearly_income);
+
+        Console.WriteLine();
+
+        double averageIncome = data
+            .Where(item => item.address.state_name == "Florida")
+            .Average(item => item.yearly_income);
+        Console.WriteLine("Average income for Florida: " + averageIncome);
+
+        Console.ReadLine();
+    }
+}
